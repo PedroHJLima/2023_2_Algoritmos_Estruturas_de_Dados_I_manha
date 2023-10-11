54a5cbfcbf5f16fd3bc3d9538218a72d34d37c35
Pedro Henrique J de Lima <54505735+PedroHJLima@users.noreply.github.com>
2023-10-11 11:10:47 -0300

    aula8

---
+++ aula08-Fila/main.py
@@ -0,0 +1,16 @@
+from Lista import Lista
+
+lista = Lista()
+
+lista.imprimir()
+lista.addInicio(10)
+lista.addInicio(5)
+lista.addFim(20)
+
+lista.removerDoInicio()
+lista.removerDoFim()
+lista.removerDoInicio()
+
+lista.adicionar(5)
+lista.adicionar(7)
+lista.adicionar(2)
